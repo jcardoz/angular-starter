b4955854c5f1fcd14bbbdcc3036fd46eb02c72d2
Jonathan Cardoz <jcardoz@deloitte.com>
2018-03-04 15:57:46 +0530

    update: Ejected the Angular App. Added the home and login routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+// Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
+// Importing the routing module
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+
+// Router import
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+import { LoginComponent } from './login/login.component';
+
+// Routes
+const appRoutes: Routes = [
+  // Empty path routes to homepage.
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  // Login.
+  {
+    path: 'login',
+    component: LoginComponent,
+  }
+];
+// End Routes
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes),
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
